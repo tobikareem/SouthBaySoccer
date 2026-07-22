cacd53598cd531b11f1573c209a8602790606fbc
tobikareem <toboibo@yahoo.com>
2026-07-22 02:11:02 -0700

    Add productionAPI for deploy

--- SouthBaySoccer/MauiProgram.cs
+++ SouthBaySoccer/MauiProgram.cs
@@ -117,7 +117,9 @@ public static class MauiProgram
         builder.Configuration.AddEnvironmentVariables();
 
         var defaultApiBaseUrl =
-#if DEBUG && ANDROID
+#if RELEASE
+            PickupPalOptions.ProductionApiBaseUrl;
+#elif DEBUG && ANDROID
             PickupPalOptions.AndroidDebugApiBaseUrl;
 #else
             PickupPalOptions.DefaultApiBaseUrl;
